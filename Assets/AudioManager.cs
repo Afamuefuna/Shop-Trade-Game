05d300349c4b505561df0ba7dd2d2bef1375c331
Simon Abugu <abugulivinusbro@gmail.com>
2021-11-21 01:16:38 +0100

    Fixes

---
+++ Assets/AudioManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    private static AudioManager _instance;
+    public static AudioManager Instance
+    {
+        get;
+        set;
+    }
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
